3cbe05eb2af41785b722908d8ebbb54bf77d21b4
dale <dalerxli@gmail.com>
2020-05-10 22:11:15 +0800

    Slider

---
+++ Slider.py
@@ -0,0 +1,39 @@
+import sys
+from PyQt5.QtWidgets import QWidget,QSlider,QLabel,QApplication
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        sld = QSlider(Qt.Horizontal,self)
+        sld.setFocusPolicy(Qt.NoFocus)
+        sld.setGeometry(30, 40, 100, 30)
+        sld.valueChanged[int].connect(self.changeValue)
+
+        self.label = QLabel(self)
+        self.label.setPixmap(QPixmap('Df.ico'))
+        self.label.setGeometry(160, 40, 80, 170)
+
+        self.setGeometry(300, 300, 280, 170)
+        self.setWindowTitle('QSlider')
+        self.show()
+
+    def changeValue(self,value):
+
+        if value==0:
+            self.label.setPixmap(QPixmap('dou.ico'))
+        elif value>0 and value<=30:
+            self.label.setPixmap(QPixmap('san.ico'))
+        elif value>30 and value<80:
+            self.label.setPixmap(QPixmap('xiao.ico'))
+        else:
+            self.label.setPixmap(QPixmap('Df.ico'))
+
+if __name__=='__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
